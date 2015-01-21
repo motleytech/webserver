540ba8595698795b3bcee5e83286e618048c6efe
motleytech <motleytechnet@gmail.com>
2015-01-21 09:08:37 -0800

    updating install setup files

--- setup/install_prerequisites.sh
+++ setup/install_prerequisites.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$#" -ne 2 ]; then
-    echo "Script requires 2 parameters. Git username and email"
+    echo -e "Script requires 2 parameters. Git username and email"
     exit 1
 fi
 
@@ -10,13 +10,13 @@ sudo apt-get -y update
 sudo apt-get -y upgrade
 
 # install diffuse
-echo "\n\n"
-echo "installing diffuse"
+echo -e "\n\n"
+echo -e "installing diffuse"
 sudo apt-get install -y diffuse
 
 # configure git
-echo "\n\n"
-echo "configuring git"
+echo -e "\n\n"
+echo -e "configuring git"
 git config --global user.name $1
 git config --global user.email $2
 git config --global alias.st "status"
@@ -29,34 +29,34 @@ git config --global diff.tool diffuse
 git config --global color.ui true
 
 # install python related stuff
-echo "\n\n"
-echo "installing python stuff"
+echo -e "\n\n"
+echo -e "installing python stuff"
 sudo apt-get install -y build-essential python-dev python-yaml \
 python-setuptools ipython pylint python-pip python-imaging virtualenvwrapper
 
 # install vim
-echo "\n\n"
-echo "installing vim"
+echo -e "\n\n"
+echo -e "installing vim"
 sudo apt-get install -y vim
 
 # install ssh server
-echo "\n\n"
-echo "installing openssh server"
+echo -e "\n\n"
+echo -e "installing openssh server"
 sudo apt-get install -y openssh-server
 
 # install postgres
-echo "\n\n"
-echo "installing postgres"
+echo -e "\n\n"
+echo -e "installing postgres"
 sudo apt-get install -y postgresql pgadmin3
 
 # final clean up
-echo "\n\n"
-echo "doing final clean up"
+echo -e "\n\n"
+echo -e "doing final clean up"
 sudo apt-get -fy install
 sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y autoclean
 sudo apt-get -y autoremove
 
-echo "\n\n"
-echo "done"
+echo -e "\n\n"
+echo -e "done"
